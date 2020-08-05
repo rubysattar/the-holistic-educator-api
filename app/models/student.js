42a0be6ae31462867bf5d8d4bdc18f1b5109ae7c
rubysattar <rubysattar@gmail.com>
2020-08-05 11:18:01 -0400

    curl scripts ready for new resource

--- app/models/student.js
+++ app/models/student.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 
-const exampleSchema = new mongoose.Schema({
-  title: {
+const studentSchema = new mongoose.Schema({
+  name: {
     type: String,
     required: true
   },
-  text: {
+  grade: {
     type: String,
     required: true
   },
@@ -18,4 +18,4 @@ const exampleSchema = new mongoose.Schema({
   timestamps: true
 })
 
-module.exports = mongoose.model('Example', exampleSchema)
+module.exports = mongoose.model('Student', studentSchema)
--- app/routes/student_routes.js
+++ app/routes/student_routes.js
@@ -4,7 +4,7 @@ const express = require('express')
 const passport = require('passport')
 
 // pull in Mongoose model for examples
-const Example = require('../models/example')
+const Student = require('../models/student')
 
 // this is a collection of methods that help us detect situations when we need
 // to throw a custom error
@@ -28,43 +28,43 @@ const requireToken = passport.authenticate('bearer', { session: false })
 const router = express.Router()
 
 // INDEX
-// GET /examples
-router.get('/examples', requireToken, (req, res, next) => {
-  Example.find()
-    .then(examples => {
-      // `examples` will be an array of Mongoose documents
+// GET /students
+router.get('/students', requireToken, (req, res, next) => {
+  Student.find()
+    .then(students => {
+      // `students` will be an array of Mongoose documents
       // we want to convert each one to a POJO, so we use `.map` to
       // apply `.toObject` to each one
-      return examples.map(example => example.toObject())
+      return students.map(student => student.toObject())
     })
     // respond with status 200 and JSON of the examples
-    .then(examples => res.status(200).json({ examples: examples }))
+    .then(students => res.status(200).json({ students: students }))
     // if an error occurs, pass it to the handler
     .catch(next)
 })
 
 // SHOW
-// GET /examples/5a7db6c74d55bc51bdf39793
-router.get('/examples/:id', requireToken, (req, res, next) => {
+// GET /students/5a7db6c74d55bc51bdf39793
+router.get('/students/:id', requireToken, (req, res, next) => {
   // req.params.id will be set based on the `:id` in the route
-  Example.findById(req.params.id)
+  Student.findById(req.params.id)
     .then(handle404)
     // if `findById` is succesful, respond with 200 and "example" JSON
-    .then(example => res.status(200).json({ example: example.toObject() }))
+    .then(student => res.status(200).json({ student: student.toObject() }))
     // if an error occurs, pass it to the handler
     .catch(next)
 })
 
 // CREATE
 // POST /examples
-router.post('/examples', requireToken, (req, res, next) => {
-  // set owner of new example to be current user
-  req.body.example.owner = req.user.id
+router.post('/students', requireToken, (req, res, next) => {
+  // set owner of new student to be current user
+  req.body.student.owner = req.user.id
 
-  Example.create(req.body.example)
-    // respond to succesful `create` with status 201 and JSON of new "example"
-    .then(example => {
-      res.status(201).json({ example: example.toObject() })
+  Student.create(req.body.student)
+    // respond to succesful `create` with status 201 and JSON of new "student"
+    .then(student => {
+      res.status(201).json({ student: student.toObject() })
     })
     // if an error occurs, pass it off to our error handler
     // the error handler needs the error message and the `res` object so that it
@@ -74,20 +74,20 @@ router.post('/examples', requireToken, (req, res, next) => {
 
 // UPDATE
 // PATCH /examples/5a7db6c74d55bc51bdf39793
-router.patch('/examples/:id', requireToken, removeBlanks, (req, res, next) => {
+router.patch('/students/:id', requireToken, removeBlanks, (req, res, next) => {
   // if the client attempts to change the `owner` property by including a new
   // owner, prevent that by deleting that key/value pair
-  delete req.body.example.owner
+  delete req.body.student.owner
 
-  Example.findById(req.params.id)
+  Student.findById(req.params.id)
     .then(handle404)
-    .then(example => {
+    .then(student => {
       // pass the `req` object and the Mongoose record to `requireOwnership`
       // it will throw an error if the current user isn't the owner
-      requireOwnership(req, example)
+      requireOwnership(req, student)
 
       // pass the result of Mongoose's `.update` to the next `.then`
-      return example.updateOne(req.body.example)
+      return student.updateOne(req.body.student)
     })
     // if that succeeded, return 204 and no JSON
     .then(() => res.sendStatus(204))
@@ -96,15 +96,15 @@ router.patch('/examples/:id', requireToken, removeBlanks, (req, res, next) => {
 })
 
 // DESTROY
-// DELETE /examples/5a7db6c74d55bc51bdf39793
-router.delete('/examples/:id', requireToken, (req, res, next) => {
-  Example.findById(req.params.id)
+// DELETE /students/5a7db6c74d55bc51bdf39793
+router.delete('/students/:id', requireToken, (req, res, next) => {
+  Student.findById(req.params.id)
     .then(handle404)
-    .then(example => {
+    .then(student => {
       // throw an error if current user doesn't own `example`
-      requireOwnership(req, example)
+      requireOwnership(req, student)
       // delete the example ONLY IF the above didn't throw
-      example.deleteOne()
+      student.deleteOne()
     })
     // send back 204 and no content if the deletion succeeded
     .then(() => res.sendStatus(204))
